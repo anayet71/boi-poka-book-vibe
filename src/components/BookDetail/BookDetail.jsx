6626527705a4499b4b5675f5e87e885affe5ebc1
anayet71 <ariyananayet@gmail.com>
2025-02-20 00:34:37 +0600

    save mark as read to locastorage

--- src/components/BookDetail/BookDetail.jsx
+++ src/components/BookDetail/BookDetail.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData, useParams } from "react-router-dom";
+import { addToStoredReadList } from "../../utility/addToDb";
 
 const BookDetail = () => {
     const {bookId} = useParams()
@@ -8,11 +9,20 @@ const BookDetail = () => {
 
     
     const book = data.find(book => book.bookId === id)
-    console.log(book,data, bookId)
+    const {bookId: currentBookId, image} = book
+
+    const handleMarkAsRead =(id) =>{
+        addToStoredReadList(id)
+    }
 
     return (
-        <div>
-            <h2>Book Details: {bookId}</h2>
+        <div className="my-5">
+            <h2>Book Details: {currentBookId}</h2>
+            <img className="w-1/5" src={image} alt="" />
+            <br />
+            <button onClick={ ()=> handleMarkAsRead(bookId)} className="btn btn-outline btn-accent mr-5">Mark as Read</button>
+            <button className="btn btn-accent">Add to Wishlist</button>
+
         </div>
     );
 };
---
+++ src/components/ListedBooks/ListedBooks.jsx
@@ -0,0 +1,10 @@
+
+const ListedBooks = () => {
+    return (
+        <div>
+            <h3 className="text-3xl"></h3>
+        </div>
+    );
+};
+
+export default ListedBooks;
